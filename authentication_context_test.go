27d2a0e9db99650b79e84dbf2972c78916e0d21c
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-30 02:23:41 +0900

    test: add Client test

--- authentication_context_test.go
+++ authentication_context_test.go
@@ -1,6 +1,9 @@
 package adal
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestNewAuthenticationContext(t *testing.T) {
 	t.Run("emptyTenant", func(t *testing.T) {
@@ -46,6 +49,58 @@ func TestNewAuthenticationContext(t *testing.T) {
 		}
 		if ac.Authority.Host != authorityHost {
 			t.Errorf("unexpected AuthorityHost. expect: %v, actual: %v", authorityHost, ac.Authority.Host)
+		}
+	})
+}
+
+func TestAuthenticationContext_Client(t *testing.T) {
+	tenant := "my.active-directory.localhost"
+	resource := "https://dummy.resource.net"
+	clientID := "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+	clientSecret := "___5oy1KvgsqA___"
+
+	ac, err := NewAuthenticationContext(tenant)
+	if err != nil {
+		t.Fatalf("AuthenticationContext construct failed: %v", err)
+	}
+
+	t.Run("emptyResource", func(t *testing.T) {
+		client, err := ac.Client(context.TODO(), "", clientID, clientSecret)
+		if err == nil {
+			t.Errorf("empty resource accepted")
+		}
+		if client != nil {
+			t.Errorf("return invalid Client")
+		}
+	})
+
+	t.Run("emptyClientID", func(t *testing.T) {
+		client, err := ac.Client(context.TODO(), resource, "", clientSecret)
+		if err == nil {
+			t.Errorf("empty clientID accepted")
+		}
+		if client != nil {
+			t.Errorf("return invalid Client")
+		}
+	})
+
+	t.Run("emptyClientSecret", func(t *testing.T) {
+		client, err := ac.Client(context.TODO(), resource, clientID, "")
+		if err == nil {
+			t.Errorf("empty clientSecret accepted")
+		}
+		if client != nil {
+			t.Errorf("return invalid Client")
+		}
+	})
+
+	t.Run("positiveCase", func(t *testing.T) {
+		client, err := ac.Client(context.TODO(), resource, clientID, clientSecret)
+		if err != nil {
+			t.Errorf("Client construct failed: %v", err)
+		}
+		if client == nil {
+			t.Errorf("return nil Client")
 		}
 	})
 }
